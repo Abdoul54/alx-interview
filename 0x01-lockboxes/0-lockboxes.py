6f88e983961898847f0a36290a0270eb79963c4e
Abdoul54 <abdelwahed.akhechane@gmail.com>
2024-01-09 08:25:46 -0800

    soy el mejor

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+from collections import deque
+
+
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+
+    Args:
+        boxes (list): List of lists representing lockboxes.
+
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
+    if not boxes or not boxes[0]:
+        return False
+
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+
+    queue = deque([0])
+
+    while queue:
+        current_box = queue.popleft()
+
+        for key in boxes[current_box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    return all(visited)
