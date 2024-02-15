f0e946173875803b79ceb3afb2c3fe06ee7eacb4
LO119 <93733505+Abdoul54@users.noreply.github.com>
2024-02-15 21:20:04 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,41 +2,16 @@
 
 const request = require('request');
 
-const id = process.argv[2];
-
-// Check if ID is provided
-if (!id) {
-  console.error('Please provide a film ID.');
-  process.exit(1);
-}
-
-request(`https://swapi-api.alx-tools.com/api/films/${id}`, (error, response, body) => {
-  if (error) {
-    console.error('Error fetching film:', error);
-    process.exit(1);
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Failed to fetch film:', response.statusCode);
-    process.exit(1);
-  }
-
-  const film = JSON.parse(body);
-
-  film.characters.forEach(url => {
-    request(url, (error, response, characterBody) => {
-      if (error) {
-        console.error(`Error fetching character ${url}:`, error);
-        return;
-      }
-
-      if (response.statusCode !== 200) {
-        console.error(`Failed to fetch character ${url}:`, response.statusCode);
-        return;
-      }
-
-      const character = JSON.parse(characterBody);
-      console.log(character.name);
-    });
-  });
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
